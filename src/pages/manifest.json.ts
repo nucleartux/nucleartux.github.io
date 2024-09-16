c3b66324e589c50ba96549a3ee5ddd58e850edcb
Adrov Igor <nucleartux@gmail.com>
2024-09-16 18:33:40 +0200

    generate favicon

---
+++ src/pages/manifest.json.ts
@@ -0,0 +1,34 @@
+import type { APIRoute } from 'astro'
+import { getImage } from 'astro:assets'
+import favicon from '../images/favicon.svg'
+import { SITE_DESCRIPTION, SITE_TITLE } from '../consts'
+
+const faviconPngSizes = [192, 512]
+
+export const GET: APIRoute = async () => {
+  const icons = await Promise.all(
+    faviconPngSizes.map(async (size) => {
+      const image = await getImage({
+        src: favicon,
+        width: size,
+        height: size,
+        format: 'png'
+      })
+      return {
+        src: image.src,
+        type: `image/${image.options.format}`,
+        sizes: `${image.options.width}x${image.options.height}`
+      }
+    })
+  )
+
+  const manifest = {
+    name: SITE_TITLE,
+    description:  SITE_DESCRIPTION,
+    start_url: '/',
+    display: 'standalone',
+    icons
+  }
+
+  return new Response(JSON.stringify(manifest))
+}
